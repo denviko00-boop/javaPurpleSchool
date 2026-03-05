d9e26831ef33643b67212cd4debd749b6f07047d
Kirill Denisevitch <denviko00@gmail.com>
2026-03-05 21:43:10 +0300

    third task

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
-const deposit = 12000
-const rate = 0.07
-const depositLength = 24
-const houseCost = 13500
+const role = 'manager'
+switch (role) {
+	case 'manager':
+		console.log('manager')
+    break
+	case 'admin':
+		console.log('admin')
+		break
 
-const res = deposit * (1 + rate / 12) ** 24
-if (res > houseCost) {
-  console.log(`You can buy this house ostatok: ${res - houseCost}`)
-}else{
-  console.log('Yoy do not have enough money')
+	default: console.log('I do not know you')
+		break
+}
+
+const num = 1
+switch (true) {
+	case num > 0:
+		console.log('Положительный')
+		break
+	case num < 0:
+		console.log('Отрицательный')
+		break
+	default: console.log('null')
+		break
+}
+
+
+const bmw = 100000
+const mazda = 20000
+const budget = 30000
+console.log(budget > bmw ? 'Покупаем бмв' : budget>mazda ? 'покупаем мазду' : 'катаемся на маршрутке')
+
+
+
+
+
+const question = prompt('Сколько будет 7+ или -15?')
+
+switch (true) {
+  case (question) === 'Я не робот':
+    case Number(question) === 22:
+    case Number(question) === -8:
+    console.log('Succesful')
+    break;
+
+  default: console.log('ТЫ РОБОТ')
+    break;
 }
